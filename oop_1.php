a070bf3c59d5f74befcb7d8853d2a2fc4efbe756
Qwintu <82965765+Qwintu@users.noreply.github.com>
2021-04-29 00:18:50 +0300

    Update oop_1.php

--- oop_1.php
+++ oop_1.php
@@ -11,7 +11,7 @@
   class Person{
     private $name;
     private $age;
-    private $hp;
+    private $hp;  // health
     private $mother;
     private $father;
     
@@ -26,17 +26,21 @@
     function getMother(){return $this->mother;}
     function getfather(){return $this->father;}
     function getName(){return $this->name;}
-    function sayHi($name){
-      echo "Привет $name, меня зовут ".$this->name;
+    function getHp(){return $this->hp."ед.";} 
+    function getAge(){return $this->age;}
+    
+    function sayHi($name,$age1){
+      echo "Привет $name, меня зовут ".$this->name.". Тебе ".$age1." лет?";
     }
-    function getHp(){return $this->hp."ед.";}
-    function setHp($hp){
+
+    function setHp($hp){   // не позволяет установить здоровье больше 100, использовалось в ранних версиях файла
       if($this->hp + $hp >= 100) $this->hp = 100;
       else $this->hp = $this->hp + $hp;
     }
+    
     function getInfo(){
       $info = "Привет, меня зовут ".$this->name."<br>
-              Мне ".$this->age." лет<br>";
+               Мне ".$this->age." лет<br>";
       if($this->mother != null){
         $info .= "Мою маму зовут ".$this->mother->getName()."<br>";
         if($this->mother->getMother() != null){
@@ -61,6 +65,9 @@
   $ivan = new Person("Иван",71); // Дедушка по папиной линии
   $oleg = new Person("Олег",34,$dasha,$ivan); // папа
   $olga = new Person("Ольга",34,$nina,$foma); // мама
-  $igor = new Person("Игорь",10,$olga,$oleg); // сын
+  $igor = new Person("Игорь",10,$olga,$oleg);
   echo $igor->getInfo();
+  // echo $oleg->sayHi($igor->name);
+  echo $igor->sayHi($ivan->getName(),$ivan->getAge());
 ?>
+
